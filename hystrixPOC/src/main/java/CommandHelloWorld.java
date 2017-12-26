6ff62d87ce861f614b55131e2c0c98492f3acaf3
deividsantos <deividfranciscosantos@hotmail.com>
2017-12-26 16:42:11 -0200

    Hello world com Hystrix

---
+++ hystrixPOC/src/main/java/CommandHelloWorld.java
@@ -0,0 +1,38 @@
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+
+
+public class CommandHelloWorld extends HystrixCommand<String> {
+
+    private final String name;
+
+    public CommandHelloWorld(String name) {
+
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        .withExecutionTimeoutInMilliseconds(2000)));
+        this.name = name;
+
+    }
+
+    @Override
+    protected String run() {
+
+
+        try {
+            Thread.sleep(1900);
+        } catch (InterruptedException e) {
+
+        }
+        return "Hello " + name + "!";
+    }
+
+    @Override
+    public String getFallback(){
+        return "Hello teste";
+    }
+
+
+}
